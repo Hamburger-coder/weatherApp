88aee579ee9693af2d0aeaeeb3e25795c15d7270
Hamburger-coder <joesmast@gmail.com>
2025-03-25 16:20:18 -0400

    Add .gitignore file, write code to fetch from api and display in console

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 // index.js
 import "./styles.css";
-import { greeting } from "./greeting.js";
 
-console.log(greeting);
-
+console.log("Start of script!")
+// Function to fetch data from the visual crossings api
+async function fetchData() {
+    console.log("Fetching data...");
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=H8D4BTVA9F9WEDCQSV5C235VX`, {mode: 'cors'});
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+fetchData();
