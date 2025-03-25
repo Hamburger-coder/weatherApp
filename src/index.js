c58b174ccbb851e3521293213414b4e156294f79
Hamburger-coder <joesmast@gmail.com>
2025-03-25 16:40:54 -0400

    Add card for weather and some css and html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 // index.js
 import "./styles.css";
 
+// variables
+const tempScale = document.getElementById("unit-btn");
+
 console.log("Start of script!")
 // Function to fetch data from the visual crossings api
 async function fetchData() {
     console.log("Fetching data...");
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=H8D4BTVA9F9WEDCQSV5C235VX`, {mode: 'cors'});
-    const data = await response.json();
-    console.log(data);
-    return data;
+//     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=H8D4BTVA9F9WEDCQSV5C235VX`, {mode: 'cors'});
+//     const data = await response.json();
+//     console.log(data);
+//     return data;
 }
 fetchData();
