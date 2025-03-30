f005ffe5a96d1bf45ad1ac5a27728a2bc9482d93
Hamburger-coder <joesmast@gmail.com>
2025-03-30 18:01:37 -0400

    Added icon image files and a bunch of other stuff

--- webpack.config.js
+++ webpack.config.js
@@ -9,23 +9,91 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    // Add publicPath to ensure proper URL resolution
+    publicPath: '/'
   },
   devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
   module: {
     rules: [
+      // JavaScript files
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      // CSS files
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      // HTML files - for processing HTML imports
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: false,
+              esModule: false
+            }
+          }
+        ]
+      },
+      // SVG files - improved approach to avoid encoding issues
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name][ext][query]'
+        }
+      },
+      // Other image files
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name][ext]'
+        }
+      }
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      filename: 'index.html'
+    }),
+  ],
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname, 'dist'),
+      },
+      {
+        directory: path.join(__dirname, 'src'),
+        publicPath: '/',
+        serveIndex: false,
+      }
+    ],
+    compress: true,
+    port: 8081,
+    hot: true,
+    // Add historyApiFallback for SPA-like behavior
+    historyApiFallback: true,
+    // Disable opening browser automatically to avoid potential issues
+    open: false,
+    // Add specific watch options
+    watchFiles: {
+      paths: ["./src/**/*"],
+      options: {
+        usePolling: false,
+      },
+    },
+    // Add dev middleware options to handle encoding issues
+    devMiddleware: {
+      writeToDisk: false,
+    },
   },
 };
 
